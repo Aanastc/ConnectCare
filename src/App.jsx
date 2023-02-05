ec5a16fab2c8b74c579416e5b3fe12a07e705990
unknown <ananovos@hotmail.com>
2023-02-04 23:22:15 -0300

    Instalando Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import "./index.css"
+import { Home } from './Pages/Home/Home'
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Home />
   )
 }
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,28 @@
 import "./Home.css"
+import Logo from "../../assets/icons/Logo.svg"
 
 export function Home() {
     return (
         <main>
-        <header>
-            <div>
-                <img src="../assets" alt="" />
-            </div>
-
-        </header>
+            <header>
+                <div className="p-6">
+                    <div>
+                        <img src={Logo} alt="Logo" />
+                    </div>
+                    <ul>
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">Sobre</a>
+                        </li>
+                        <li>
+                            <a href="">Serviços</a>
+                        </li>
+                    </ul>
+                </div>
+            </header>
         </main>
+
     )
 }
