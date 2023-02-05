46586bf384d70fa8b45f2cbefc58c0ee602bfa90
unknown <ananovos@hotmail.com>
2023-02-04 22:34:46 -0300

    tirando o componente que o Miqueias implicou

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,9 +3,12 @@ import "./Home.css"
 export function Home() {
     return (
         <main>
+        <header>
             <div>
-
+                <img src="../assets" alt="" />
             </div>
+
+        </header>
         </main>
     )
 }
