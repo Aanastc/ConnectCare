2592c8cf8a676433ceb688010dcc1f25a3d3d62b
unknown <ananovos@hotmail.com>
2023-02-24 16:17:39 -0300

    a

--- src/Pages/FaleConosco.jsx
+++ src/Pages/FaleConosco.jsx
@@ -1,5 +1,5 @@
-import { Header } from '../Components/Header';
-import { Footer } from '../Components/Footer';
+import { Header } from '../Components/Header'
+import { Footer } from '../Components/Footer'
 import ImgFaleConosco from '../assets/imgs/img-faleConosco.png'
 
 export function FaleConosco() {
@@ -7,24 +7,18 @@ export function FaleConosco() {
     <section>
       <Header />
       <div className="grid grid-cols-2 gap-36">
-        <div className="flex flex-col justify-center gap-6">
-          <div>
-            <img src={ImgFaleConosco} alt="Imagem de uma personagem indicando a seção ao lado" />
+        <div className="flex flex-col gap-6">
+          <div className="items-center">
+            <img
+              src={ImgFaleConosco}
+              alt="Imagem de uma personagem indicando a seção ao lado"
+              className="items-center"
+            />
           </div>
         </div>
-        <div className='flex flex-row content-center'>
-          <h2 className='font-bold text-3xl items-center'>Fale Conosco</h2>
-          <form className='items-start'>
-            <input type="text" />
-            <label htmlFor="">Nome</label>
-            <input type="email" />
-            <label htmlFor="">Email</label>
-            <input type="text" />
-            <label htmlFor="">Mensagem</label>
-          </form>
-          <button>
-            Enviar
-          </button>
+        <div>
+          <h2 className="font-bold text-3xl items-center">Fale Conosco</h2>
+          <form></form>
         </div>
       </div>
       <Footer />
