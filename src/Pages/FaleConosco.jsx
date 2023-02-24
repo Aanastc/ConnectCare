99692dc92bcc83944087b7cd536fc1c9d293c144
ana l <ananovos@htomail.com>
2023-02-24 11:40:36 -0300

    mudanças page fale conosco

--- src/Pages/FaleConosco.jsx
+++ src/Pages/FaleConosco.jsx
@@ -1,20 +1,33 @@
 import { Header } from '../Components/Header';
 import { Footer } from '../Components/Footer';
-// import imgFaleConosco from '../assests/imgs/img-faleConosco.png'
+import ImgFaleConosco from '../assets/imgs/img-faleConosco.png'
 
 export function FaleConosco() {
   return (
-    <main>
+    <section>
       <Header />
-      <div>
-        <div>
-          {/* <img src={imgFaleConosco} alt="Imagem de uma personagem indicando a seção ao lado" /> */}
+      <div className="grid grid-cols-2 gap-36">
+        <div className="flex flex-col justify-center gap-6">
+          <div>
+            <img src={ImgFaleConosco} alt="Imagem de uma personagem indicando a seção ao lado" />
+          </div>
         </div>
-        <div>
-          <h2>Fale Conosco</h2>
+        <div className='flex flex-row content-center'>
+          <h2 className='font-bold text-3xl items-center'>Fale Conosco</h2>
+          <form className='items-start'>
+            <input type="text" />
+            <label htmlFor="">Nome</label>
+            <input type="email" />
+            <label htmlFor="">Email</label>
+            <input type="text" />
+            <label htmlFor="">Mensagem</label>
+          </form>
+          <button>
+            Enviar
+          </button>
         </div>
       </div>
       <Footer />
-    </main>
+    </section>
   )
 }
