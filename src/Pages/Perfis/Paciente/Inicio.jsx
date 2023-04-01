61bb8e1308411b44362e07fdf7df1e03b155f343
jferraz <jferraz79@gmail.com>
2023-04-01 14:55:26 -0300

    Criação de sidebar em Inicio.jsx

--- src/Pages/Perfis/Paciente/Inicio.jsx
+++ src/Pages/Perfis/Paciente/Inicio.jsx
@@ -1,9 +1,33 @@
-import {HeaderApp} from '../Componetes/HeaderApp'
+import logo from '../../../assets/icons/LogoRoxa.svg'
+import { ArrowLeft, Stack, ChatCircleDots, CalendarCheck } from 'phosphor-react'
 
 export function Inicio() {
+  
   return (
-    <div>
-        <HeaderApp/>
-    </div>
+    <nav className='w-56 fixed left-0 top-0 h-screen bg-slate-50 p-3' >
+      {/*<HeaderApp/>*/}
+       <div className='flex flex-row justify-start items-center gap-2 mb-4'>
+        <img src={logo} alt="logo roxa"/>
+        <p className='text-lg text-purple-600 font-medium'>Connect Care</p>
+        <ArrowLeft size={24} color="#9063cd" />
+      </div>
+      <div>
+        <ul className='p-3'>
+          <li className='flex flex-row gap-2 mb-4'>
+            <Stack size={24} />
+            <p>Visao Geral</p>
+          </li>
+          <li className='flex flex-row gap-2 mb-4'>
+            <ChatCircleDots size={24} />
+            <p>Chat</p>
+          </li>
+          <li className='flex flex-row gap-2'>
+            <CalendarCheck size={24} />
+            <p>Servicos</p>
+          </li>
+        </ul>
+      </div>             
+    </nav>
+    
     )
 }
