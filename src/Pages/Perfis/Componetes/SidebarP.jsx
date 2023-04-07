06964c0ed8a6d466d3b6339e548fd5643b6ef674
JorgeBorges <jorge.borges@parceiro.gruposaltaedu.com>
2023-04-07 12:16:09 -0300

    Atualização sidebar da tela de início

--- src/Pages/Perfis/Componetes/SidebarP.jsx
+++ src/Pages/Perfis/Componetes/SidebarP.jsx
@@ -10,31 +10,18 @@ import { useState } from 'react'
 
 export function SidebarP() {
   const [open, setOpen] = useState(true)
-  const Menus = [
-  { title: "Visão Geral", src:"1"},
-  { title: "Chat"},
-  { title: "Serviços"},
-  
-];
+ 
 
   return (
     <nav
-      className={`relative left-0 top-0 h-screen bg-slate-50 p-3 transition-all duration-300 ${
-        open ? 'w-56' : 'w-20'
-      }`}
+      className={`left-0 top-0 h-screen bg-slate-100 p-5 pt-8 relative duration-300 ${open ? 'w-72' : 'w-28'}`}
     >
       {open ? (
         <>
           <div className="flex flex-row justify-start items-center gap-x-4 mb-4">
             <img src={logo} alt="logo roxa" className={`cursor-pointer duration-500 ${
               open && 'rotate-[360deg]'}`} />
-            <h1
-            className={`text-purple-600 origin-left font-medium text-xl duration-200 ${
-              !open && "scale-0"
-            }`}
-          >
-            Connect Care
-          </h1>
+            <h1 className={`text-purple-600 origin-left font-medium text-xl duration-500 ${!open && "scale-50" }`}>Principal</h1>
             <button
               className="absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
               border-2 rounded-full"
@@ -45,27 +32,23 @@ export function SidebarP() {
           </div>
           <div>
             <ul className="p-3">
-            {Menus.map((Menu, index) => (
+            
               <>
-                <li key={index} 
-                className={`flex flex-row gap-2 mb-4 rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
-                ${Menu.gap ? "mt-9" : "mt-2"} ${index === 0 && "bg-light-white"}`}
-                >
-                  {/* <Stack size={24} />
+                <li className={`flex gap-2 mb-4 rounded-md p-2 cursor-pointer hover:bg-light-white text-black text-sm items-center gap-x-4 ${!open && "scale-150" }`}>
+                   <Stack size={24} />
                     <p>Geral</p>
-                </li><li className="flex flex-row gap-2 mb-4">
+                </li><li className={`flex flex-row gap-2 mb-4 rounded-md p-2 cursor-pointer hover:bg-light-white text-black text-sm items-center gap-x-4 ${!open && "scale-50" }`}>
                     <ChatCircleDots size={24} />
                     <p>Chat</p>
-                </li><li className="flex flex-row gap-2">
+                </li><li className="flex flex-row gap-2 rounded-md p-2 cursor-pointer hover:bg-light-white text-black text-sm items-center gap-x-4">
                     <CalendarCheck size={24} />
-                    <p>Servicos</p> */}
-                    <img src={`./../../../assets/icons/${Menu.src}.svg`} />
-                    <span className={`${!open && "hidden"} origin-left duration-200`}>
-                      {Menu.title}
+                    <p>Servicos</p>
+                    
+                    <span className={`${!open && "hidden"} origin-left duration-500`}>                      
                     </span>
                 </li>
               </>
-            ))}
+            
             </ul>
           </div>
         </>
@@ -84,25 +67,23 @@ export function SidebarP() {
           </div>
           <div>
           <ul className="p-3">
-            {Menus.map((Menu, index) => (
+            
               <>
-                <li key={index} 
-                className={`flex flex-row gap-2 mb-4 rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
-                ${Menu.gap ? "mt-9" : "mt-2"} ${index === 0 && "bg-light-white"}`}
-                >
-                {/* <Stack size={24} />
+                <li className={`flex flex-row gap-2 mb-4 p-2 cursor-pointer hover:bg-light-white text-black text-xs items-center gap-x-4`}>
+                <Stack size={24} />
                     
-                </li><li className="flex flex-row gap-2 mb-4">
-                    <ChatCircleDots size={24} />
+                </li><li className="flex flex-row gap-2 mb-4 rounded-md p-2 cursor-pointer hover:bg-light-white text-black text-sm items-center gap-x-4">
+                <ChatCircleDots size={24} />
                     
-                </li><li className="flex flex-row gap-2">
-                    <CalendarCheck size={24} /> */}
-                <span className={`${!open && "hidden"} origin-left duration-200`}>
-                  {Menu.title}
+                </li><li className="flex flex-row gap-2 rounded-md p-2 cursor-pointer hover:bg-light-white text-black text-sm items-center gap-x-4">
+                <CalendarCheck size={24} />
+
+                <span className={`${!open && "hidden"} origin-left duration-500`}>
+                  
                 </span>   
                 </li>
               </>
-            ))}
+            
             </ul>
           </div>
         </>
