a67752d6dca45311ef5bae7e831a3fa2cdc8fd1f
Ana <ananovos@hotmail.com>
2023-04-13 13:16:52 -0300

    organizando page paciente

---
+++ src/Pages/Perfis/Paciente/Conteudo.jsx
@@ -0,0 +1,15 @@
+import { Card } from './Card'
+
+export function Conteudo() {
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+        tempore obcaecati repudiandae, voluptate praesentium fugit, pariatur a
+        nam ipsam quae ex ut dolor odit aperiam quibusdam inventore laboriosam
+        quam! Quam!
+      </p>
+      <Card />
+    </main>
+  )
+}
