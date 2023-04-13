bae23e794f040d1bdb1465ec6f7b6f3a0bc07d0c
johnmonteir <johngoes14@gmail.com>
2023-04-13 18:11:22 -0300

    deu

--- src/Pages/Perfis/Paciente/Conteudo.jsx
+++ src/Pages/Perfis/Paciente/Conteudo.jsx
@@ -1,15 +1,47 @@
 import { Card } from './Card'
+import React from 'react';
 
 export function Conteudo() {
   return (
     <main>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
-        tempore obcaecati repudiandae, voluptate praesentium fugit, pariatur a
-        nam ipsam quae ex ut dolor odit aperiam quibusdam inventore laboriosam
-        quam! Quam!
-      </p>
+      <div className="bg-purple-400 rounded-lg overflow-hidden p-6">
+        {/* Conteúdo do card */}
+        <img src="src/assets/animation/Bem-vindo-animate.svg" alt="Imagem de boas-vindas" width='35vw'/>
+        <h1 className="text-2xl font-bold mb-2 text-black">Bem-vindo(a) John Goes Monteir</h1>
+      </div>
+      <div className="my-4 flex justify-center">
+        <button className="bg-white border-2 border-purple-400 rounded flex justify-center items-center h-[80px] w-[80px] mx-2">
+          <i className="icon"></i>
+          <span className="ml-2">Neonatal</span>
+        </button>
+        <button className="bg-white border-2 border-purple-400 rounded flex justify-center items-center h-[80px] w-[80px] mx-2">
+          <i className="icon"></i>
+          <span className="ml-2">internação</span>
+        </button>
+        <button className="bg-white border-2 border-purple-400 rounded flex justify-center items-center h-[80px] w-[80px] mx-2">
+          <i className="icon"></i>
+          <span className="ml-2">Vacinas</span>
+        </button>
+        <button className="bg-white border-2 border-purple-400 rounded flex justify-center items-center h-[80px] w-[80px] mx-2">
+          <i className="icon"></i>
+          <span className="ml-2">Idoso</span>
+        </button>
+        <button className="bg-white border-2 border-purple-400 rounded flex justify-center items-center h-[80px] w-[80px] mx-2">
+          <i className="icon"></i>
+          <span className="ml-2">Cuicado paliativo</span>
+        </button>
+        <button className="bg-white border-2 border-purple-400 rounded flex justify-center items-center h-[80px] w-[80px] mx-2">
+          <i className="icon"></i>
+          <span className="ml-2">Curativo</span>
+        </button>
+        <button className="bg-white border-2 border-purple-400 rounded flex justify-center items-center h-[80px] w-[80px] mx-2">
+          <i className="icon"></i>
+          <span className="ml-2">Medicação</span>
+        </button>
+        
+      </div>
       <Card />
     </main>
   )
 }
+
