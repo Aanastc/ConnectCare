3ffb9c461f52612ea5a3232aa418bb5d2f374364
Ana <ananovos@hotmail.com>
2023-04-14 15:52:09 -0300

    componente saudacao

--- src/Pages/Perfis/Paciente/Conteudo.jsx
+++ src/Pages/Perfis/Paciente/Conteudo.jsx
@@ -1,7 +1,5 @@
-import { UserContext } from '../../../contexts/UserCtx'
-import { useState, useEffect } from 'react'
-import { useContext } from 'react'
 import { Filtro } from '../Componetes/Filtro'
+import { Saudacao } from '../Componetes/saudacao'
 import { Card } from './Card'
 import {
   Baby,
@@ -14,43 +12,13 @@ import {
   Pill
 } from 'phosphor-react'
 
-import homePaciente from '../../../../src/assets/icons/homePaciente.svg'
-
 export function Conteudo() {
-  const { metadata } = useContext(UserContext)
-  const [horario, setHorario] = useState(new Date().getHours())
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setHorario(new Date().getHours())
-    }, 1000 * 60)
-    return () => clearInterval(interval)
-  }, [])
-
-  let saudacao
-  if (horario >= 6 && horario < 12) {
-    saudacao = 'Bom dia'
-  } else if (horario >= 12 && horario < 18) {
-    saudacao = 'Boa tarde'
-  } else {
-    saudacao = 'Boa noite'
-  }
-
   const especialidade =
     'border-purple-400 border-2 rounded flex flex-col justify-center items-center h-24 w-24 m-2 p-2 hover:bg-purple-400 hover:shadow-md'
 
   return (
     <main className="p-5 pt-32">
-      <div className="flex flex-row justify-center items-center bg-purple-600 rounded-full shadow-md w-full h-20 mb-4">
-        <img
-          src={homePaciente}
-          alt=""
-          className="flex items-start absolute top-0 left-28"
-        />
-        <h1 className="text-3xl font-normal text-black flex items-center uppercase">
-          {saudacao}, {metadata?.name}!
-        </h1>
-      </div>
+      <Saudacao />
       <div className="flex flex-row mb-4 w-full gap-5">
         <button className={especialidade}>
           <Baby size={32} color="#0a0a0b" />
@@ -86,7 +54,16 @@ export function Conteudo() {
         </button>
       </div>
       <Filtro />
-      <Card />
+      <div className="flex flex-wrap gap-10">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </main>
   )
 }
