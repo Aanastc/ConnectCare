120e263160720f356ed233b5d1189a78aee1386e
Ana <ananovos@hotmail.com>
2023-04-28 14:08:13 -0300

    estilo aparição do card profissional

--- src/Pages/Perfis/Profissional/VisaoGeralProf.jsx
+++ src/Pages/Perfis/Profissional/VisaoGeralProf.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Saudacao } from '../Componetes/Saudacao'
 import { CardAtendimento } from './CardAtendimento'
-// import { CardFinalizados } from './CardFinalizados'
+import { CardFinalizados } from './CardFinalizados'
 import { supabase } from '../../../services/supabase'
 import { useUser } from '../../../contexts/UserCtx'
 
@@ -36,30 +36,31 @@ export function VisaoGeralProf() {
     <main className="p-5 pt-32">
       <Saudacao />
       <h1 className="font-medium text-2xl mb-4">Atendimentos em andamento:</h1>
-      <div className="flex flex-wrap gap-10 justify-center mb-4">
-        {paciente.map(pacienteEmAtendimento => (
-          <CardAtendimento
-            nome={pacienteEmAtendimento.name}
-            casoClinico={pacienteEmAtendimento?.paciente?.casoClinico}
-            horario={pacienteEmAtendimento.horario}
-            dias={pacienteEmAtendimento.dias}
-          />
-        ))}
-      </div>
-      <h1 className="font-medium text-2xl mb-4">Atendimentos finalizados:</h1>
-      <span className="text-gray-400 text-sm">
-        Ainda não há atendimentos finalizados
-      </span>
+      {paciente.length ? (
+        <div className="flex flex-wrap gap-10 justify-center mb-4">
+          {paciente.map(pacienteEmAtendimento => (
+            <CardAtendimento
+              nome={pacienteEmAtendimento.name}
+              casoClinico={pacienteEmAtendimento?.paciente?.casoClinico}
+              horario={pacienteEmAtendimento.horario}
+              dias={pacienteEmAtendimento.dias}
+            />
+          ))}
+        </div>
+      ) : (
+        <span className="text-gray-400 text-sm">Ainda não há atendimentos</span>
+      )}
+      {/* <h1 className="font-medium text-2xl mb-4">Atendimentos finalizados:</h1>
       <div className="flex flex-wrap gap-10 justify-center">
-        {/* {pacientes.map(pacienteEmFinalizado => (
+        {paciente.map(pacienteEmFinalizado => (
           <CardFinalizados
             nome={pacienteEmFinalizado.nome}
-            casoClinico={pacienteEmFinalizado.casoClinico}
+            casoClinico={pacienteEmFinalizado?.paciente?.casoClinico}
             horario={pacienteEmFinalizado.horario}
             dias={pacienteEmFinalizado.dias}
           />
-        ))} */}
-      </div>
+        ))}
+      </div> */}
     </main>
   )
 }
