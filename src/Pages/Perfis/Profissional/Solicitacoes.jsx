46e0e29f137ed6ad7b911d0d86235ed73ba77cc2
Miqueias Sousa <miqueiassousa80@gmail.com>
2023-04-28 15:53:12 -0300

    feat: add message

--- src/Pages/Perfis/Profissional/Solicitacoes.jsx
+++ src/Pages/Perfis/Profissional/Solicitacoes.jsx
@@ -102,77 +102,75 @@ export function Solicitacoes() {
     <div className="p-2 mb-4">
       <h1 className="font-medium text-2xl mb-4">Solicitaçõees</h1>
       <div className="flex flex-col p-6 border border-purple-500 rounded-lg shadow-lg justify-center">
-        {solicitacaoPacientes.length
-          ? solicitacaoPacientes.map(item => (
-              <div
-                className="border border-purple-500 rounded-lg flex justify-between p-4 mb-4 pr-10"
-                key={item.id}
-              >
-                <div>
-                  <h2 className="text-purple-400 text-2xl font-semibold">
-                    {item.paciente.profiles.name}
-                  </h2>
-                  <p className="font-semibold">
-                    Diagnóstico:
-                    <span className="font-normal"> {item.diagnostico}</span>
-                  </p>
-                  <p className="font-semibold">
-                    Horário:
-                    <span className="font-normal"> {item.horario}</span>
-                  </p>
-                  <p className="font-semibold">
-                    Dias:
-                    <span className="font-normal">
-                      {' '}
-                      {item.dias?.join(' - ')}
-                    </span>
-                  </p>
-                  <p className="font-semibold">
-                    Período do contrato:
-                    <span className="font-normal">
-                      {' '}
-                      {`${differenceInDays(
-                        new Date(item.periodo_final),
-                        new Date(item.periodo_inicial)
-                      )} dias`}
-                    </span>
-                  </p>
-                  <p className="font-semibold">
-                    Pernoite:
-                    <span className="font-normal"> {item.pernoite}</span>
-                  </p>
-                  <p className="font-semibold">
-                    Endereço:
-                    {/* ainda não tem */}
-                    {/* <span className="font-normal"> {item.Endereco}</span> */}
-                  </p>
-                  <p className="font-semibold">
-                    Observações:
-                    {/* ainda não tem */}
-                    {/* <span className="font-normal"> {item.Observacoes}</span> */}
-                  </p>
-                </div>
-                <div className="flex flex-col justify-center items-center gap-6">
-                  <button
-                    className="bg-purple-400 text-white rounded-lg px-2 py-1 w-56 font-light text-sm hover:bg-purple-600"
-                    onClick={() => aceitarContrato(item.id)}
-                  >
-                    ACEITAR
-                  </button>
-                  <button
-                    className="bg-purple-400 text-white rounded-lg px-2 py-1 w-56 font-light text-sm hover:bg-purple-600"
-                    onClick={() => rejeitarContrato(item.id)}
-                  >
-                    REJEITAR
-                  </button>
-                  <button className="bg-purple-400 text-white rounded-lg px-2 py-1 w-56 font-light text-sm hover:bg-purple-600">
-                    CONVERSAR COM PACIENTE
-                  </button>
-                </div>
+        {solicitacaoPacientes.length ? (
+          solicitacaoPacientes.map(item => (
+            <div
+              className="border border-purple-500 rounded-lg flex justify-between p-4 mb-4 pr-10"
+              key={item.id}
+            >
+              <div>
+                <h2 className="text-purple-400 text-2xl font-semibold">
+                  {item.paciente.profiles.name}
+                </h2>
+                <p className="font-semibold">
+                  Diagnóstico:
+                  <span className="font-normal"> {item.diagnostico}</span>
+                </p>
+                <p className="font-semibold">
+                  Horário:
+                  <span className="font-normal"> {item.horario}</span>
+                </p>
+                <p className="font-semibold">
+                  Dias:
+                  <span className="font-normal"> {item.dias?.join(' - ')}</span>
+                </p>
+                <p className="font-semibold">
+                  Período do contrato:
+                  <span className="font-normal">
+                    {' '}
+                    {`${differenceInDays(
+                      new Date(item.periodo_final),
+                      new Date(item.periodo_inicial)
+                    )} dias`}
+                  </span>
+                </p>
+                <p className="font-semibold">
+                  Pernoite:
+                  <span className="font-normal"> {item.pernoite}</span>
+                </p>
+                <p className="font-semibold">
+                  Endereço:
+                  {/* ainda não tem */}
+                  {/* <span className="font-normal"> {item.Endereco}</span> */}
+                </p>
+                <p className="font-semibold">
+                  Observações:
+                  {/* ainda não tem */}
+                  {/* <span className="font-normal"> {item.Observacoes}</span> */}
+                </p>
               </div>
-            ))
-          : null}
-        {}
+              <div className="flex flex-col justify-center items-center gap-6">
+                <button
+                  className="bg-purple-400 text-white rounded-lg px-2 py-1 w-56 font-light text-sm hover:bg-purple-600"
+                  onClick={() => aceitarContrato(item.id)}
+                >
+                  ACEITAR
+                </button>
+                <button
+                  className="bg-purple-400 text-white rounded-lg px-2 py-1 w-56 font-light text-sm hover:bg-purple-600"
+                  onClick={() => rejeitarContrato(item.id)}
+                >
+                  REJEITAR
+                </button>
+                <button className="bg-purple-400 text-white rounded-lg px-2 py-1 w-56 font-light text-sm hover:bg-purple-600">
+                  CONVERSAR COM PACIENTE
+                </button>
+              </div>
+            </div>
+          ))
+        ) : (
+          <span>Não há solicitações pendentes</span>
+        )}
       </div>
     </div>
   )
