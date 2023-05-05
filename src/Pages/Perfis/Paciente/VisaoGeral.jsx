8fc6d376b1e09d79bb478ece3df002393f5bf7b9
Ana <ananovos@hotmail.com>
2023-05-05 10:00:34 -0300

    corrigindo a page de visão geral

--- src/Pages/Perfis/Paciente/VisaoGeral.jsx
+++ src/Pages/Perfis/Paciente/VisaoGeral.jsx
@@ -1,42 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { Baby, Bed, FaceMask, FirstAidKit, Heartbeat, Pill, Syringe, Wheelchair } from 'phosphor-react';
-import { Filtro } from './Filtro';
-import { Saudacao } from '../Componetes/Saudacao';
-import { Card } from './Card';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import {
+  Baby,
+  Bed,
+  FaceMask,
+  FirstAidKit,
+  Heartbeat,
+  Pill,
+  Syringe,
+  Wheelchair
+} from 'phosphor-react'
+import { Filtro } from './Filtro'
+import { Saudacao } from '../Componetes/Saudacao'
+import { Card } from './Card'
+import { NavLink } from 'react-router-dom'
+import { supabase } from '../../../services/supabase'
 
 export function VisaoGeral() {
-  const [showProfileNotification, setShowProfileNotification] = useState(false);
+  const [profissionais, setProfissionais] = useState([])
 
   useEffect(() => {
-    const profileNotificationDismissed = localStorage.getItem('profileNotificationDismissed');
-    setShowProfileNotification(!profileNotificationDismissed);
-  }, []);
-
-  function handleProfileNotificationClick() {
-    localStorage.setItem('profileNotificationDismissed', 'true');
-    setShowProfileNotification(false);
-  }
-
-  const profissionais = [
-    {
-      name: 'Julia de Sousa',
-      especialidade: 'Cuidadora de idosos',
-      atendimentos: '12'
-    },
-    {
-      name: 'Antony Marques',
-      especialidade: 'Cuidadora Neonatal',
-      atendimentos: '8'
+    async function getProfissionais() {
+      const { data, error } = await supabase
+        .from('profiles')
+        .select(
+          `
+          id,
+          name,
+          profissional (
+            especialidade
+          )`
+        )
+        .eq('role', 'caregiver')
+      if (error) console.log('Erro ao buscar profissionais:', error)
+      else setProfissionais(data)
     }
-  ]
+    getProfissionais()
+  }, [])
+
+  // const [showProfileNotification, setShowProfileNotification] = useState(false);
+
+  // useEffect(() => {
+  //   const profileNotificationDismissed = localStorage.getItem('profileNotificationDismissed');
+  //   setShowProfileNotification(!profileNotificationDismissed);
+  // }, []);
+
+  // function handleProfileNotificationClick() {
+  //   localStorage.setItem('profileNotificationDismissed', 'true');
+  //   setShowProfileNotification(false);
+  // }
 
   const especialidade =
     'border-purple-400 border-2 rounded flex flex-col justify-center items-center h-24 w-24 m-2 p-2 hover:bg-purple-400 hover:shadow-md'
 
   return (
     <main className="p-5 pt-32">
-      {showProfileNotification && (
+      {/* {showProfileNotification && (
         <div className="bg-yellow-100 p-4 mb-4 rounded-md">
           <p className="text-yellow-800">
             Por favor, complete seu perfil para ter acesso completo à plataforma.
@@ -45,11 +63,11 @@ export function VisaoGeral() {
             className="bg-yellow-500 text-white px-4 py-2 rounded-md mt-2 hover:bg-yellow-600"
             to="/Paciente/editarPerfil"
             onClick={handleProfileNotificationClick}
-           >
+          >
             Completar perfil
           </NavLink>
         </div>
-      )}
+      )} */}
       <Saudacao />
       <div className="flex flex-row mb-4 w-full gap-5 justify-center items-center">
         <button className={especialidade}>
@@ -89,8 +107,9 @@ export function VisaoGeral() {
       <div className="flex flex-wrap gap-12 justify-center">
         {profissionais.map(perfilDoProfissional => (
           <Card
+            profissionalId={perfilDoProfissional.id}
             name={perfilDoProfissional.name}
-            especialidade={perfilDoProfissional.especialidade}
+            especialidade={perfilDoProfissional?.profissional?.especialidade}
             atendimentos={perfilDoProfissional.atendimentos}
           />
         ))}
