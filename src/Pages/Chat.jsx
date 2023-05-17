0cfbe5410a7b52a9056b3b03f3435e9ca7bf8ab4
Ana <ananovos@hotmail.com>
2023-05-17 15:01:55 -0300

    iniciando o chat

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import { supabase } from '../services/supabase'
+import React, { useState, useEffect } from 'react'
 import { useUser } from '../contexts/UserCtx'
 import { PaperPlaneRight, Smiley, Paperclip } from 'phosphor-react'
 
@@ -13,49 +14,82 @@ export function Chat() {
     setInputValue(e.target.value)
   }
 
-  const handleSendMessage = () => {
+  const handleSendMessage = async () => {
     if (inputValue.trim() !== '') {
       const newMessage = {
         content: inputValue,
         sender: user.name
       }
-      setMessages([...messages, newMessage])
-      setInputValue('')
+
+      const { data, error } = await supabase.from('chat').insert(newMessage) // Insere a nova mensagem na tabela 'chat'
+
+      if (error) {
+        console.error('Erro ao enviar a mensagem:', error)
+      } else {
+        setMessages([...messages, newMessage])
+        setInputValue('')
+      }
     }
   }
 
-  // colocar para pegar o id do perfil
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const { data, error } = await supabase
+        .from('chat')
+        .select('*')
+        .order('id') // Lê todas as mensagens da tabela 'chat'
+
+      if (error) {
+        console.error('Erro ao buscar as mensagens:', error)
+      } else {
+        setMessages(data)
+      }
+    }
+
+    fetchMessages()
+
+    const subscription = supabase
+      .from('chat')
+      .on('INSERT', payload => {
+        setMessages([...messages, payload.new])
+      })
+      .subscribe()
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [messages])
 
   return (
-    <main>
-      <div className="border border-purple-500 rounded-lg shadow-lg w-full max-w-[1215px]">
+    <main className="flex justify-center items-center h-screen">
+      <div className="border border-purple-500 rounded-lg shadow-lg w-full max-w-[1235px]">
         <div className="flex flex-row items-center gap-4 border-b-2 border-b-purple-500 p-2">
           <img
             className="w-10 h-10 rounded-full ring-2 ring-gray-300"
-            src={user.avatar}
+            src={user.avatarPath}
             alt={user.name}
           />
           <p className="text-lg font-medium text-purple-500">{user.name}</p>
         </div>
-        <div className="w-full">
+        <div className="overflow-y-auto max-h-[500px] bg-gray-100">
           {messages.map((message, index) => (
             <div
               key={index}
-              className={`flex m-2 break-words ${
+              className={`flex my-2 ${
                 message.sender === user.name ? 'justify-end' : 'justify-start'
               }`}
             >
               <div className="flex items-center">
-                {!message.sender === user.name && (
-                  <div className="flex-shrink-0 bg-purple-500 text-white rounded-full h-8 w-8 flex items-center justify-center">
-                    <span className="text-sm font-bold">A</span>
+                {message.sender !== user.name && (
+                  <div className="flex-shrink-0 bg-purple-500 text-white rounded-br-3xl rounded-tr-3xl rounded-tl-xl h-8 w-8 flex items-center justify-center">
+                    <span className="text-sm font-bold">{user.name}</span>
                   </div>
                 )}
               </div>
               <div
                 className={`rounded-lg py-2 px-4 ml-2 ${
                   message.sender === user.name
-                    ? 'bg-gray-200 border border-purple-500 max-w-[490px] h-full'
+                    ? 'bg-gray-200 border border-purple-500 max-w-[490px] h-full rounded-bl-3xl rounded-tl-3xl rounded-tr-xl'
                     : 'bg-gray-800 text-white'
                 }`}
               >
@@ -68,28 +102,19 @@ export function Chat() {
           <div className="flex-1 pr-2">
             <div className="relative">
               <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
-                <Smiley
-                  size={32}
-                  weight="thin"
-                  className="hover:text-purple-500"
-                />
-                <Paperclip
-                  size={32}
-                  weight="thin"
-                  className="hover:text-purple-500"
-                />
+                <Smiley size={32} />
               </span>
               <input
                 type="text"
-                className="w-full pl-20 p-2 border rounded-2xl focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent"
+                className="w-full pl-12 p-2 border rounded-2xl focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent"
                 placeholder="Digite uma mensagem..."
                 value={inputValue}
                 onChange={handleInputChange}
               />
             </div>
           </div>
-          <button onClick={handleSendMessage}>
-            <PaperPlaneRight size={32} className="hover:text-purple-500" />
+          <button className="hover:text-purple-500" onClick={handleSendMessage}>
+            <PaperPlaneRight size={32} />
           </button>
         </div>
       </div>
