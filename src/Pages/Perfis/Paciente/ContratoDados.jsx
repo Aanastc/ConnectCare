b7e38740e81f0f0aec3798fa003019c728059734
Miqueias Sousa <miqueiassousa80@gmail.com>
2023-05-25 19:10:32 -0300

    fix: use form context to share data between components

--- src/Pages/Perfis/Paciente/ContratoDados.jsx
+++ src/Pages/Perfis/Paciente/ContratoDados.jsx
@@ -1,11 +1,35 @@
 import React, { useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { NavLink, useParams } from 'react-router-dom'
+import { Check } from 'phosphor-react'
+import { useUser } from '../../../contexts/UserCtx'
+import { supabase } from '../../../services/supabase'
 import { ContratoInfos } from './ContratoInfos'
 import { DadosCobranca } from './DadosCobranca'
 import { Pagamento } from './Pagamento'
-import { Check } from 'phosphor-react'
 
 export function ContratoDados() {
+  const [showModal, setShowModal] = useState(false)
   const [currentStep, setCurrentStep] = useState(1)
+  const methods = useForm()
+  const { id } = useParams()
+  const { user } = useUser()
+
+  async function handleSubmit(data) {
+    await supabase.from('contrato').insert({
+      horario: 'Integral',
+      pernoite: 'Sim',
+      periodo_inicial: data.periodo_inicial,
+      periodo_final: data.periodo_final,
+      dias_semana: ['Segunda', 'Quarta', 'Sexta'],
+      diagnostico: data.diagnostico,
+      descricao: data.descricao,
+      profissinal_id: id,
+      paciente_id: user?.id
+    })
+
+    setShowModal(true)
+  }
 
   const handleNextStep = () => {
     setCurrentStep(prevStep => prevStep + 1)
@@ -36,6 +60,7 @@ export function ContratoDados() {
           <Pagamento
             onNextStep={handleNextStep}
             onPreviousStep={handlePreviousStep}
+            onSubmit={handleSubmit}
           />
         )
       default:
@@ -87,9 +112,40 @@ export function ContratoDados() {
 
       <div className="flex flex-row gap-6 mb-4 justify-center">
         <div className="flex flex-col p-6 border border-purple-500 rounded-lg shadow-lg">
-          {renderStepContent()}
+          <FormProvider {...methods}>{renderStepContent()}</FormProvider>
         </div>
       </div>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="rounded-lg shadow-lg relative flex flex-col w-full bg-purple-100 outline-none focus:outline-none">
+                <div className="relative p-6 flex-auto">
+                  <p className="text-slate-500 font-medium text-lg">
+                    PARABÉNS!!!
+                  </p>
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    Nós da Connect Care agradecemos a confiança. <br />
+                    Sua solicitação foi enviada com sucesso, você pode encontrar
+                    informações sobre o status dela na barra lateral.
+                  </p>
+                </div>
+                <div className="flex justify-end p-2">
+                  <NavLink to="/Paciente/visaoGeral">
+                    <button
+                      className="bg-purple-500 text-white rounded-full h-10 w-64"
+                      onClick={() => setShowModal(false)}
+                    >
+                      Voltar para home
+                    </button>
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </div>
   )
 }
