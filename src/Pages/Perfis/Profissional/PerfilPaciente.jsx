b2ed076a39e894ce9d8757d3aa868cdfb0a177c9
claradibe <claradibe@hotmail.com>
2023-06-02 15:30:21 -0300

    perfil  paciente

--- src/Pages/Perfis/Profissional/PerfilPaciente.jsx
+++ src/Pages/Perfis/Profissional/PerfilPaciente.jsx
@@ -1,8 +1,13 @@
-export function PerfilPaciente(){
+export function PerfilPaciente() {
   return (
-    <main>
-      <div>
-        <p>Teste</p>
+    <main className="flex justify-center">
+      <div className="flex justify-between items-center">
+        <div className="border-2 border-purple-600 rounded-full w-72 h-72 shadow-md shadow-purple-500">
+          <img src="" className="rounded-full w-full"  />
+        </div>
+        <div className="flex flex-col p-6 border border-purple-500 rounded-lg shadow-lg">
+
+        </div>
       </div>
     </main>
   )
